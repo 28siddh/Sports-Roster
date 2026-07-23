6636926800ccf28467af2a00e378a6860e880f7a
Siddh Patel <psiddh2812@gmail.com>
2026-07-23 18:02:26 +0530

    feat: add User model with pre-save password hashing hook

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 const express = require("express");
 const connectDB = require('./config/db');
 const requestLogger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 
 connectDB();
 
 const app = express();
 
+app.get('/boom', (req, res, next) => next(new Error('test')))
+
 app.use(express.json());
 app.use(requestLogger);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
