d026540978f8ff0a84184cf07ed01dadf92d1cf7
Siddh Patel <psiddh2812@gmail.com>
2026-07-23 17:17:02 +0530

    feat: add custom request logging middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require("express");
 const connectDB = require('./config/db');
+const requestLogger = require('./middleware/logger');
 
 connectDB();
 
 const app = express();
+
 app.use(express.json());
+app.use(requestLogger);
 
 const PORT = process.env.PORT || 5000;
 
