f534da6416fe1647dfb49f82d33f30be6bab681f
Siddh Patel <psiddh2812@gmail.com>
2026-07-23 18:34:32 +0530

    feat: add Player model matching Step 0 schema

---
+++ backend/models/player.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['batsman', 'bowler', 'all-rounder', 'wicketkeeper']
+    },
+    availibiltyStatus: {
+        type: Boolean,
+        default: true,
+    },
+    battingStyle: {
+        type: String,
+        enum: ['right-handed', 'left-handed'],
+    },
+    bowlingStyle: {
+        type: String,
+        enum: ['right-handed', 'left-handed', 'pure-batsman'],
+    },
+});
+
+module.exports = mongoose.model('Player', playerSchema);
